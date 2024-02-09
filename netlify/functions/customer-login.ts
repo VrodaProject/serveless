a781090a71a584e8aa75b81ab0090ae84db1ff85
Sin-god <illya.komarnickii1@gmail.com>
2024-02-09 14:32:48 +0200

    got fix

--- netlify/functions/customer-login.ts
+++ netlify/functions/customer-login.ts
@@ -54,29 +54,3 @@ const handler: Handler = async (event, context) => {
 };
 
 export { handler };
-
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Set environment variables for your credentials
-// Read more at http://twil.io/secure
-// const accountSid = "AC0442b593ede736bc4f4e0cbb4bc23467";
-// const authToken = "2f37cdd325fa6c8a04d3aafbce1dfdfa";
-// const verifySid = process.env.YOUR_VERIFY_SID;
-// const client = require("twilio")(accountSid, authToken);
-
-// client.verify.v2
-//   .services(verifySid)
-//   .verifications.create({ to: "+380976597611", channel: "sms" })
-//   .then((verification) => console.log(verification.status))
-//   .then(() => {
-//     const readline = require("readline").createInterface({
-//       input: process.stdin,
-//       output: process.stdout,
-//     });
-//     readline.question("Please enter the OTP:", (otpCode) => {
-//       client.verify.v2
-//         .services(verifySid)
-//         .verificationChecks.create({ to: "+380976597611", code: otpCode })
-//         .then((verification_check) => console.log(verification_check.status))
-//         .then(() => readline.close());
-//     });
-//   });
